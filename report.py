eebe26ce0a24fa5f2b560073eefe9acac7088316
clementchueng <57971469+clementchueng@users.noreply.github.com>
2021-04-25 01:08:53 +0800

    Update report.py

--- report.py
+++ report.py
@@ -1,76 +1,25 @@
-# 通过 SMTP 发送邮件
+# 将IMAP获取的邮件内容发送到gTTS合成
+# -*- coding: utf-8 -*-
 
-from email.header import Header
-from email.mime.text import MIMEText
-from email.utils import parseaddr, formataddr
-import smtplib
+import getEmail
+import getOA
+from gtts import gTTS
 
+unseen_msg = getEmail.getEmail()
 
-# 格式化邮箱地址
-def format_adds(s):
-    name, addr = parseaddr(s)
-    # 如果name含有中文，则将其编码进行格式化
-    return formataddr((Header(name, 'utf-8').encode(), addr))
+words = '您现在共有' + str(unseen_msg['nums']) + '封未读邮件,'
 
+if unseen_msg['nums'] > 0:
+    for e in unseen_msg.keys():
+        if isinstance(unseen_msg[e], dict):
+            words += unseen_msg[e]['From'] + '给你给来了一封主题为' + \
+                unseen_msg[e]['Subject'] + '的邮件,'
 
-# 发送邮件
-def sendEmail(mail_type, from_addr, password, to_addr, subject, content):
-    # 设置SMTP服务器及发件人名
-    sender = ''
-    smtp_server = ''
-    if mail_type == 'gmail':  # gmail
-        sender = from_addr[0:-10]
-        # gmail采用SSL加密方式
-        smtp_server = 'smtp.gmail.com'
+            if 'Contant' in unseen_msg[e]:
+                words += '内容为,' + unseen_msg[e]['Content']
 
-    elif mail_type == 'outlook':  # outlook
-        sender = from_addr[0:-11]
-        # outlook采用 STARTTLS 加密方式
-        smtp_server = 'smtp.partner.outlook.cn'
+            if 'Attachment' in unseen_msg[e]:
+                words += '还有一个附件,' + unseen_msg[e]['Attachment']
 
-    else:
-        print('不支持', mail_type, '邮箱')
-
-    port = 587
-    server = smtplib.SMTP(smtp_server, port)
-    server.ehlo()
-    server.starttls()
-
-    # 收件人名
-    if to_addr[-9:-4] == 'gmail':
-        receiver = to_addr[0:-10]
-    else:
-        receiver = to_addr[0:-11]
-
-    if isinstance(content, str):
-        msg = MIMEText(content, 'plain', 'utf-8')
-    else:
-        msg = MIMEText(content, 'html', 'utf-8')
-    msg['From'] = format_adds(sender + '<%s>' % from_addr)
-    print(msg['From'])
-    msg['To'] = format_adds(receiver + '<%s>' % to_addr)
-    print(msg['To'])
-    msg['Subject'] = Header(subject, 'utf-8').encode()
-
-    # 连接SMTP服务器,发送邮件
-    server.set_debuglevel(1)  # 打印与SMTP服务器交互的信息
-    server.login(from_addr, password)
-    server.sendmail(from_addr, to_addr, msg.as_string())
-
-    # 关闭连接
-    server.quit()
-
-
-# 只在该模块内运行输入邮箱邮件信息
-if __name__ == '__main__':
-    # 输入邮箱信息
-    mail_type = input('邮箱类型：')
-    from_addr = input('邮箱地址：')
-    password = input('邮件密码：')
-    to_addr = input('收件人地址：')
-    # 文本邮件信息
-    subject = input('邮件主题：')
-    content = input('邮件内容：')
-    sendEmail(mail_type, from_addr, password, to_addr, subject, content)
-else:
-    pass
+tts = gTTS(words,lang='zh-TW')
+tts.save('email.mp3')
